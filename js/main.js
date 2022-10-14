7df6fe871813bb991dd59ffd1a6c1bba8386b11f
Роман Боронин <r.vboronin@gmail.com>
2022-10-14 20:53:42 +0300

    добавляет объект описание фотографии

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const NUMBER_OBJECTS = 25;
+const NUMBER_LIKES = {
+  min: 15,
+  max: 200
+}
+
 //Функция возвращает случайное целое число из переданного диапазона включительно:
 
 function returnsRandomNumber (min, max) {
@@ -15,3 +21,17 @@ function checksLengthString (stringToTest, maximumLengthString) {
   return (stringToTest.length <= maximumLengthString);
 
 }
+
+
+//Создание объекта описание фотографии:
+
+const photoDescription = {
+  id: '',
+  url: '',
+  description: '',
+  likes: returnsRandomNumber(NUMBER_LIKES.min, NUMBER_LIKES.max),
+  comments: ''
+}
+
+console.log(photoDescription.likes);
+
