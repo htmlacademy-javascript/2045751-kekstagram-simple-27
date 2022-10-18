f4a76d0cef0a08e80ed35e0cf1dbe18e69878a73
Роман Боронин <r.vboronin@gmail.com>
2022-10-18 13:13:25 +0300

    добавляет файлы модулей js

---
+++ js/util.js
@@ -0,0 +1,27 @@
+//Функция для проверки максимальной длины строки:
+
+function checksLengthString (stringToTest, maximumLengthString) {
+
+  return (stringToTest.length <= maximumLengthString);
+
+}
+
+//Функция возвращает случайное целое число из переданного диапазона включительно:
+
+function returnsRandomNumber (min, max) {
+
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return ( (min >= 0 && min < max) && (!isNaN(min) && !isNaN(max) ) ) ? randomNumber : NaN;
+
+}
+
+//Функция вовращает случайный элемент массива:
+
+function returnsRandomElementArray(element) {
+
+  return element[returnsRandomNumber(0, element.length - 1) ];
+
+}
+
+export {returnsRandomElementArray, returnsRandomNumber};
