28f607696860282525f8ae0b5316d8bf540b9fbb
Роман Боронин <r.vboronin@gmail.com>
2022-11-03 21:19:59 +0300

    добавляет функции открытия и закрытия окна редактирования фото

--- js/form.js
+++ js/form.js
@@ -1,10 +1,37 @@
-//Получаем форму:
-
 const form = document.querySelector('.img-upload__form');
+const uploadFile = form.querySelector('.img-upload__input');
+const uploadOverlay = form.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const buttonCancel = form.querySelector('.img-upload__cancel');
+
+function showPopup() {
+
+  uploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+
+}
+
+function closePopup() {
+
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+}
+
+uploadFile.addEventListener('change', () => {
+
+  showPopup();
+
+});
+
+buttonCancel.addEventListener('click', () => {
+
+  closePopup();
+
+});
 
 const pristine = new Pristine(form, {
   classTo: 'text',
   errorTextParent: 'text',
   errorTextClass: 'text__errorText'
-
 });
